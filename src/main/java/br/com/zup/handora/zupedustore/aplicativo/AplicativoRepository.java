c5c3a6dc2ececf09e3014e21073ecd105cd6e7be
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-25 00:40:27 -0300

    Add aplicativo repository

---
+++ src/main/java/br/com/zup/handora/zupedustore/aplicativo/AplicativoRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.handora.zupedustore.aplicativo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AplicativoRepository extends JpaRepository<Aplicativo, Long> {
+
+}
